d66d5ea29b1f57e5fa0f1a22fc23ddb5c6d08414
Neous1 <Neous1@users.noreply.github.com>
2017-08-11 11:46:44 -0400

    setup geolocation

--- script.js
+++ script.js
@@ -7,14 +7,31 @@ jQuery(document).ready(function ($) {
       var state = parsed_json['location']['state'];
       var temp_f = parsed_json['current_observation']['temp_f'];
       var temp_c = parsed_json['current_observation']['temp_c'];
-      alert("Current temperature in " + city + " , " + state + " is: " + temp_f + "f  " + temp_c + "c");
+      // alert("Current temperature in " + city + " , " + state + " is: " + temp_f + "f  " + temp_c + "c");
+      var temp = document.getElementById("temp").innerHTML = "Current temperature in " + city + " , " + state + " is: " + temp_f + "f  " + temp_c + "c";
     }
+
+
   });
+// determine the user's location
+  x = navigator.geolocation;
+  x.getCurrentPosition(success, failure);
+
+  function success(position) {
+    debugger;
+    var mylat = position.coords.latitude;
+    var mylong = position.coords.longitude;
+    $("#lat").html(mylat);
+    $("#long").html(mylong);
+
+  }
+
+  function failure() {
+    $('#lat').html("<p> no coordinates</p>")
+  }
+
+
+
+
+
 });
-
-
-// if(jQuery){
-//   alert("Yes")
-// }else{
-//   alert("no jquery found")
-// }
